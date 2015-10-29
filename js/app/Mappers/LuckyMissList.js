bfc80eb78d6a1fb693515ea63d16b4ea88278c5c
rythos42 <rythos42@gmail.com>
2015-10-29 14:01:14 -0700

    Data updates

---
+++ js/app/Mappers/LuckyMissList.js
@@ -0,0 +1,47 @@
+/*globals C, Upgrade */
+/*exported LuckyMissList */
+var LuckyMissList = {
+	data: [
+		new Upgrade('Martyr', [C.Guild], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('In the Name of Research', [C.Guild], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Reltentless', [C.Guild], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Secret Directive', [C.Guild], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Deep Pockets', [C.Guild], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		
+		new Upgrade('My Lucky Stitches', [C.Resurrectionist], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Too Many Pieces', [C.Resurrectionist], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Embalmed', [C.Resurrectionist], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Pounce', [C.Resurrectionist], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Spirit', [C.Resurrectionist], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		
+		new Upgrade('Hydraulic Limb', [C.Arcanist], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Armor', [C.Arcanist], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('December\'s Hunger', [C.Arcanist], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Bestial Rage', [C.Arcanist], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Steamborg Executioner', [C.Arcanist], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		
+		new Upgrade('To The Death', [C.Neverborn], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Silent', [C.Neverborn], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Ravenous', [C.Neverborn], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Slippery', [C.Neverborn], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Doppleganger', [C.Neverborn], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		
+		new Upgrade('The Hunter', [C.Outcast], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('The Scholar', [C.Outcast], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('The Scavenger', [C.Outcast], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('The Murderer', [C.Outcast], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('The Chosen', [C.Outcast], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		
+		new Upgrade('Stop Hitting Yourself!', [C.Gremlin], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Lowered Expectations', [C.Gremlin], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Twitchy', [C.Gremlin], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Fast Runner', [C.Gremlin], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Scapegoat', [C.Gremlin], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		
+		new Upgrade('Out of the Fire', [C.TenThunders], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Arcane Disturbance', [C.TenThunders], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Clear Mind, Pure Body', [C.TenThunders], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Reserves', [C.TenThunders], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0),
+		new Upgrade('Ninja Attack!', [C.TenThunders], [C.Upgrade, C.Campaign, C.LuckyMiss, C.Wave3], 0)
+	]
+};
--- js/app/Mappers/UpgradeListMapper.js
+++ js/app/Mappers/UpgradeListMapper.js
@@ -2,6 +2,6 @@
 /*exported UpgradeListMapper */
 var UpgradeListMapper = {
 	get: function() {
-		return UpgradeList.data.concat(EquipmentList.data, SkillList.data, InjuryList.data);
+		return UpgradeList.data.concat(EquipmentList.data, SkillList.data, InjuryList.data, LuckyMissList.data);
 	}
 };
