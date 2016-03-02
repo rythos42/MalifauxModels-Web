b7f10e006071170d4a704d0d81df1786a128513b
rythos42 <Blyrble7>
2016-03-02 10:44:57 -0800

    Updating to Cordova 4.1.1, making tabs fixed

--- js/app/CustomBinders/LoadMoreCustomBinder.js
+++ js/app/CustomBinders/LoadMoreCustomBinder.js
@@ -1,9 +1,10 @@
 /*globals _, ko */
 ko.bindingHandlers.loadMore = {
 	init: function(elementDom, valueAccessor, allBindings, viewModel) {
-		var window = $(document);
-		window.scroll(_.throttle(function() {
-			if(window.height() - window.scrollTop() - document.body.clientHeight <= 100) {
+        // Only loads when #full-list is the scrolling element
+        var scrollParent = $(elementDom).closest('#full-list');
+		scrollParent.scroll(_.throttle(function() {
+			if(scrollParent[0].scrollHeight - scrollParent[0].scrollTop - scrollParent[0].clientHeight <= 100) {
 				viewModel.addNextModelItems();
 			}
 		}, 100));
